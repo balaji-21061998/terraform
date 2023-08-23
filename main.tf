b64c83cf16a3e1c4a2e36154a43d9361ee6144fd
balaji-21061998 <139453672+balaji-21061998@users.noreply.github.com>
2023-08-23 17:07:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
-resource "local_file" "my_team" {
-filename = var.filename
-content = var.content
+resource "local_file" "pet" {
+filename = "pets.txt"
+content = "My cat is ${random_pet.mypet.id}"    
+} 
+resource "random_pet" "mypet" {
+prefix = "MR"
+separator = "."
+length = "1"
+}
+output "mypet" {
+  value = random_pet.mypet.id
 }
