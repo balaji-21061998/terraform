090182bd4d248fac4d946fec25ba0108478a562d
balaji-21061998 <139453672+balaji-21061998@users.noreply.github.com>
2023-08-24 18:34:24 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,4 +1,1 @@
-AWS Access Key ID [****************DTUG]: AKIARJA6VZR4LAEMKWZS
-AWS Secret Access Key [****************6z8U]: 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
-Default region name [us-east-1]: us-west-1
-Default output format [json]: 
+
